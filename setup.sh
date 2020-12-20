5a267e1a94bdad8068ec3fccbde62c28ae5fc5ad
Eric Ngo <quocdat32461997@yahoo.com>
2020-12-19 21:42:44 -0600

    Clean

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# initialize virtual environment
-!virutalenv emorecoom
-source ./emorecom/bin/activate
-chmod 777 ./emorecom/bin/activate
-
-# install depeendencies
-pip3 install -r setup_requirements.txt
