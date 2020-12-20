017a1be5eaa80af2519ed5cd75dcbc04f9422c5d
Eric Ngo <quocdat32461997@yahoo.com>
2020-12-19 20:36:58 -0600

    First commits too download-warmup-dataset

---
+++ download_warmup_dataset.sh
@@ -0,0 +1,8 @@
+#!/bin/ash
+
+# install warm-up dataset
+wget --no-check-certificate 'https://docs.google/com/uc?export=download&id=1CLBhXp1I2h7kqifwILYvumtEpf4UiRu4' -O ./dataset/warm-up-train.zip
+#wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ZpY8Bh9_UKwBVIhuiyCgZesLwhwqpvFO' -O ./dataset/warm-up-test.zip
+
+#unzip ./dataset/warm-up-train.zip -d ./dataset/warm-up-train
+#unzip ./dataset/warm-up-test.zip -d ./dataset/warm-up-test
