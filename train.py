6ceb239259b38d8f9627dc58eb748e6c6f31859f
Eric Ngo <quocdat32461997@yahoo.com>
2020-12-21 22:51:38 -0600

    Clean

--- train.py
+++ train.py
@@ -6,27 +6,12 @@ train.py - training module
 import os
 import glob
 import argparse
-import neptune
 import tensorflow as tf
-from neptunecontrib.monitoring.keras import NeptuneMonitor
 
 # import local packages
 from emorecom.data import Dataset
 
-# read neptune token
-with open('neptune_token.txt') as file:
-	api_token = file.read()
-
-# initialize neptune logging
-neptune.init(
-	api_token = api_token,
-	project_qualified_name = 'ericngo/emorecom/')
-neptune.set_project('ericngo/emorecom')
-
 def train(args):
-
-	# create experiment
-	neptune.create_experiment('model') # change to your model name
 
 	# initialize train dataset
 	train_path = args.data_path
@@ -40,9 +25,6 @@ def train(args):
 	# test train-dataset
 	sample = next(iter(train_data))
 	print(sample)
-
-
-	neptune.stop()
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser('Argument Parser')
