fee991c1591cd4910e140b9cc667e586b3d233aa
Eric Ngo <quocdat32461997@yahoo.com>
2020-12-21 23:47:53 -0600

    Added vision & text modules

--- emorecom/model.py
+++ emorecom/model.py
@@ -9,3 +9,15 @@ from tensorflow.keras import Input, Model
 
 def create_model(configs):
 	return Model(inputs = inputs, outputs = outputs)
+
+def vision():
+	"""
+	vision - function to create visual module
+	"""
+	return Model(inputs, outputs = outputs)
+
+def text():
+	"""
+	text - function to create textual module
+	"""
+	return Model(inputs = inputs, outputs = outputs)
