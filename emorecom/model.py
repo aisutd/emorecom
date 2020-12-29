e4827ae3520471d3d1c7a82eaebe4094e032093a
quocdat32461997 <quocdat32461997@yahoo.com>
2020-12-29 15:32:19 -0600

    Commit

--- emorecom/model.py
+++ emorecom/model.py
@@ -48,7 +48,7 @@ def create_model(configs):
 	outputs = Dense(100, activation = 'relu')(outputs)
 	outputs = Flatten()(outputs)
 	outputs = Dense(50, activation = 'relu')(outputs)
-	outputs = Dense(configs['num_class'], activation = 'softmax')(outputs)
+	outputs = Dense(configs['num_class'], activation = 'sigmoid')(outputs)
 	return Model(inputs = [vision_model.inputs, text_model.inputs],
 		outputs = outputs)
 
--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # run following command to start training
-python3 train.py --experiment-name text-only-v2 --num-class 8 \
+python3 train.py --experiment-name text-only-v2-sigmoid --num-class 8 \
 --text-len 128 --image-height 224 --image-width 224 \
---embedding-dim None --batch-size 16 --learning-rate 0.0001 \
+--embedding-dim None --batch-size 16 --learning-rate 0.001 \
 --epochs 10 --vocab-size None --vocabs dataset/vocabs.txt \
 --train-data dataset/train.tfrecords \
 --logdir logs --checkpoint-dir checkpoints \
