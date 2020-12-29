77f522abefa09fc2ff40b907e78c412bb3700267
quocdat32461997 <quocdat32461997@yahoo.com>
2020-12-28 23:28:23 -0600

    Move Twitter-Glove to the main dir

--- download_twitter_glove_we.sh
+++ download_twitter_glove_we.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # download twitter glove word embeddings
-cd dataset
 wget http://nlp.stanford.edu/data/glove.twitter.27B.zip
 
 # unzip Twitter Glove Word Embeddings
-unzip glove.twitter.27B.zip
+unzip glove.twitter.27B.zip -d glove.twitter.27B
