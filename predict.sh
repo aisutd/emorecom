06d18ae5d57689b75c10de2293b832c622048a74
quocdat32461997 <quocdat32461997@yahoo.com>
2021-01-04 23:00:20 -0600

    Fixs

--- predict.sh
+++ predict.sh
@@ -5,7 +5,7 @@ python3 train.py --experiment-name model-1 --num-class 8 \
 --text-len 128 --image-height 224 --image-width 224 \
 --embedding-dim None --batch-size 16 \
 --vocab-size None --vocabs dataset/vocabs.txt \
---train-data dataset/train.tfrecords \
+--test-data dataset/test.tfrecords \
 --logdir logs --checkpoint-dir checkpoints \
 --pretrained-embedding glove.twitter.27B/glove.twitter.27B.100d.txt \
 --saved-models 'saved_models'
