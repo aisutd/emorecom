ab06058eb3751c856f9e81b730878b9f68d12977
quocdat32461997 <quocdat32461997@yahoo.com>
2021-01-04 23:51:09 -0600

    Clean

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@
 python3 train.py --experiment-name text-only-v1-sigmoid --num-class 8 \
 --text-len 128 --image-height 224 --image-width 224 \
 --embedding-dim None --batch-size 16 --learning-rate 0.001 \
---epochs 1 --vocab-size None --vocabs dataset/vocabs.txt \
+--epochs 10 --vocab-size None --vocabs dataset/vocabs.txt \
 --train-data dataset/train.tfrecords \
 --logdir logs --checkpoint-dir checkpoints \
 --pretrained-embedding glove.twitter.27B/glove.twitter.27B.100d.txt \
