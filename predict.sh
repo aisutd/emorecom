1ef3b9c440dd229b7c9218442c4052ecc35e50b8
quocdat32461997 <quocdat32461997@yahoo.com>
2021-02-03 09:28:34 -0600

    Fixed args parsing

--- predict.sh
+++ predict.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # run following command to start training
-python3 train.py --experiment-name model-1 --num-class 8 \
+python3 predict.py --experiment-name model --num-class 8 \
 --text-len 128 --image-height 224 --image-width 224 \
 --embedding-dim None --batch-size 16 \
 --vocab-size None --vocabs dataset/vocabs.txt \
 --test-data dataset/test.tfrecords \
---logdir logs --checkpoint-dir checkpoints \
 --pretrained-embedding glove.twitter.27B/glove.twitter.27B.100d.txt \
 --saved-models 'saved_models'
